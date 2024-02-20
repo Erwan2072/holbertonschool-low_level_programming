8c0b54a313ed093f4b8245030433ded78f83edeb
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-20 12:36:13 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - 5-print_numbers.c
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n < 0; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
