d0c55040749fb76d31164515bbd8276fb90623d6
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-20 13:08:51 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - 6-print_numberz.c
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int number = 0;
+for(number = 0; number <= 9; number++)
+putchar('0'+number);
+putchar("\n");
+return (0);
+}
+  
