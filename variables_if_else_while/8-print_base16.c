7f6fb7051c2af29b77a3bd8f808d53386e8b4596
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-20 14:37:34 +0000

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - 8-print_base16.c
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int number;
+char alph;
+
+for (number = '0'; number <= '9'; number++)
+putchar(number);
+for (alph = 'a' ; alph <= 'f' ; alph++)
+putchar(alph)
+
+return (0);
+}
