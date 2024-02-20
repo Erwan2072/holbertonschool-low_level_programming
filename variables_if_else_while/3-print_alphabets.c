9762b1bb54f1b1d602f01ebd1e4ffd55e588e8bb
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-20 09:56:13 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -13,13 +13,10 @@ int main(void)
 int alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++;)
-{
 putchar(alphabet);
-}
 for (alphabet = 'A'; alphabet <= 'A'; alphabet++;)
-{
 putchar(alphabet);
-}
+
 putchar('\n')
 
 return (0);
