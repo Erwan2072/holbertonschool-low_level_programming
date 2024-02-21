5d2da29b2bb5e5014a4e3a50e0313288191c1a24
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-21 12:50:02 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - x10
+ *that prints 10 times the alphabet followed by a new line
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i = 9;
+
+	while (i <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
