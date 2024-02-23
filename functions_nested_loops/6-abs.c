00f6f6a4c87f409cce24657ce438fd0d8a25b17d
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-23 08:07:51 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*int_abs - calucle la valeur absolue
+*@n : lien vers la variable
+*Return: Always 0 (Success)
+*/
+
+int _abs(int n);
+{
+	if (n <= 0)
+		return (-n);
+	else
+		return (n);
+}
