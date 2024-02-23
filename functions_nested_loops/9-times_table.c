561d53bdc129233e09a7d2093be9357d3e384658
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-23 10:23:35 +0000

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ *Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int x; y; z;
+
+	for (x = 0 ; x <= ; x++)
