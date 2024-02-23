103c31679aefc45a660becf13a52301eacf1a17d
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-23 13:48:33 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - adds two integers and returns the result
+ *@a:additionne a b
+ *@b:additionne a a
+ *Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
