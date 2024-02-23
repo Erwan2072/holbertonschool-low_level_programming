52b8c10eeb8052bb9d5c4442693c4e41563d9476
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-23 11:49:15 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,6 +8,34 @@
 
 void times_table(void)
 {
-	int x; y; z;
+	int x;
+	int y;
+	int z;
 
-	for (x = 0 ; x <= ; x++)
+	for (x = 0 ; x <= 10 ; x++)
+	{
+		for (y = 0 ; y < 10 ; y++)
+		{
+			z = x * y;
+			if (x == 0)
+			{
+				_putchar(z + '0');
+			}
+
+			if (z < 10 && x != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			} else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
