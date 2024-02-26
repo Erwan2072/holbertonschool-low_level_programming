435374bc628fda9297869013d992d224f158f3f2
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-26 09:58:51 +0000

    2-mulk.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*mul -  multiplies two integers
+*@a : is variable
+*@b : is variable
+*Return: Always 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (0);
+}
