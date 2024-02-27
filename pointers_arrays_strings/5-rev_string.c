c83aab753ba1362da42d2f02dab074b3ee4bee2b
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-02-27 14:07:37 +0000

    5-rev_string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,4 +7,23 @@
 
 void rev_string(char *s)
 {
-	
+	char tmp;
+	int i, j, k;
+
+	j = 0;
+	k = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+
+	k = j - 1;
+
+	for (i = 0; i < j / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[k];
+		s[k--] = tmp;
+	}
+}
