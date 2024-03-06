8978e0d500330548aec2bdaa1ebd11b514eb53b5
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-03-06 08:02:03 +0000

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,8 +4,8 @@
  * _strchr - locates a character in a string
  * @s: the string to check
  * @c: the character we're looking for
- * Return: a pointer to the first occurance of the character @c in the string
- * @s. Return NULL if the character isn't found
+  Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
  */
 
 char *_strchr(char *s, char c)
