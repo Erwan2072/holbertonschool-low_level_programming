f9b5d3a65b4bd3121a4faad3235458dbdfcd407c
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-03-06 13:43:00 +0000

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,16 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc >= 3)
-	{
-		int n1 = atoi(argv[1]);
-		int n2 = atoi(argv[2]);
-		int result = n1 * n2;
-		printf("%d\n", result);
-	}
-	else
-	{
-		printf("Error\n");
-	}
-	return (0);
+if (argc >= 3)
+{
+int n1 = atoi(argv[1]);
+int n2 = atoi(argv[2]);
+int result = n1 * n2;
+
+printf("%d\n", result);
+}
+else
+{
+printf("Error\n");
+}
+return (0);
 }
