5b5f5c7d08629404a1f280093cb5863b2f211ad2
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-03-12 14:54:03 +0000

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -19,10 +19,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb)
+	for (i = 0; i < nmemb; i++)
 		p[i] = 0;
-	i++;
-	
+
 	return (p);
 	free(p);
 }
