3eee9856d5bd9aec39476387571fb566f24829c7
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-03-13 13:44:41 +0000

    task4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
