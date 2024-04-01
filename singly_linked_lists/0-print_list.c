ae0a4a934d245cad499a53737b92b27809521f66
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-01 10:17:54 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,7 +11,9 @@ size_t print_list(const list_t *h)
 
 	c = 0;
 	ptr = h;
-	while (ptr != NULL)
+	if (ptr != NULL)
+		printf("[0](nil)\n");
+	else
 	{
 		printf("[%d] %s\n", ptr->len, ptr->str);
 		ptr = ptr->next;
