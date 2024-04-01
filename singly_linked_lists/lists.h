c3627352b95df8b664863add3595f4a168f19030
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-01 08:45:42 +0000

    modif struct

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -13,11 +13,11 @@
  * Description: singly linked list node structure
  */
 
-typedef struct list_s
+typedef struct lists
 {
     char *str;
     unsigned int len;
-    struct list_s *next;
+    struct lists *next;
 } list_t;
 
 size_t print_list(const list_t *h);
