473da817aed37aeb8ff3b067735376f983d16b32
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-01 10:30:56 +0000

    list.h

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,22 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	/* Initialize the counter for nodes */
-	size_t nodes = 0;
-	/* Traverse the list until we reach the end */
-	while (h)
-	{
-		/* Check if the string is NULL */
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			/* Print the length and string */
-			printf("[%d] %s\n", h->len, h->str);
-		/* Increment the node counter */
-		nodes++;
-		/* Move to the next element of the list */
-		h = h->next;
-	}
-	/* Return the total number of nodes in the list */
-	return (nodes);
+        const list_t *ptr;
+        size_t c;
+
+        c = 0;
+        ptr = h;
+        while (ptr != NULL)
+        {
+                printf("[%d] %s\n", ptr->len, ptr->str);
+                ptr = ptr->next;
+                c++;
+        }
+
+        return (c);
 }
