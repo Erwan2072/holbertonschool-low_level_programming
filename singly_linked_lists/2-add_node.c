820ada954e4a8fac440db842c7aa3cf8ff58c8a8
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-01 09:04:29 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,15 +8,15 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_head = malloc(sizeof(list_t));
 
-	if (head == NULL || new_node == NULL  || str == NULL)
+	if (head == NULL || new_head == NULL  || str == NULL)
 	return (0);
 
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+	new_head->str = strdup(str);
+	new_head->len = strlen(str);
+	new_head->next = *head;
+	*head = new_head;
 
-	return (new_node);
+	return (new_head);
 }
